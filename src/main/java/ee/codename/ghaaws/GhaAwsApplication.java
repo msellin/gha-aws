9ba2ba919459a6b9267d88c5879e6c86fa9a6278
Margus Sellin <sellinmargus@gmail.com>
2024-02-23 10:42:28 +0200

    Initial commit

---
+++ src/main/java/ee/codename/ghaaws/GhaAwsApplication.java
@@ -0,0 +1,13 @@
+package ee.codename.ghaaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/codename/ghaaws/GhaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.codename.ghaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
